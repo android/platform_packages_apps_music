1de034d9a510a18562636e1fa6601ae56d51458c
Lucas Gomez Jimenez <lucas.gomezjimenez@tomtom.com>
2015-07-30 13:46:02 +0200

    Fixed Audio Focus handling on Music application
    The app was not handling the situation where the audio focus
    request was denied.
    
    Change-Id: Ib94260788837bb774562021b6d113e79c9c2d6e2

--- src/com/android/music/MediaPlaybackService.java
+++ src/com/android/music/MediaPlaybackService.java
@@ -1154,36 +1154,38 @@ public class MediaPlaybackService extends Service {
      * Starts playback of a previously opened file.
      */
     public void play() {
-        mAudioManager.requestAudioFocus(mAudioFocusListener, AudioManager.STREAM_MUSIC,
+        int res = mAudioManager.requestAudioFocus(mAudioFocusListener, AudioManager.STREAM_MUSIC,
                 AudioManager.AUDIOFOCUS_GAIN);
-        mAudioManager.registerMediaButtonEventReceiver(new ComponentName(this.getPackageName(),
-                MediaButtonIntentReceiver.class.getName()));
+        if (res == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            mAudioManager.registerMediaButtonEventReceiver(new ComponentName(this.getPackageName(),
+                    MediaButtonIntentReceiver.class.getName()));
 
-        if (mPlayer.isInitialized()) {
-            // if we are at the end of the song, go to the next song first
-            long duration = mPlayer.duration();
-            if (mRepeatMode != REPEAT_CURRENT && duration > 2000 &&
-                mPlayer.position() >= duration - 2000) {
-                gotoNext(true);
+            if (mPlayer.isInitialized()) {
+                // if we are at the end of the song, go to the next song first
+                long duration = mPlayer.duration();
+                if (mRepeatMode != REPEAT_CURRENT && duration > 2000 &&
+                    mPlayer.position() >= duration - 2000) {
+                    gotoNext(true);
+                }
+
+                mPlayer.start();
+                // make sure we fade in, in case a previous fadein was stopped because
+                // of another focus loss
+                mMediaplayerHandler.removeMessages(FADEDOWN);
+                mMediaplayerHandler.sendEmptyMessage(FADEUP);
+
+                updateNotification();
+                if (!mIsSupposedToBePlaying) {
+                    mIsSupposedToBePlaying = true;
+                    notifyChange(PLAYSTATE_CHANGED);
+                }
+
+            } else if (mPlayListLen <= 0) {
+                // This is mostly so that if you press 'play' on a bluetooth headset
+                // without every having played anything before, it will still play
+                // something.
+                setShuffleMode(SHUFFLE_AUTO);
             }
-
-            mPlayer.start();
-            // make sure we fade in, in case a previous fadein was stopped because
-            // of another focus loss
-            mMediaplayerHandler.removeMessages(FADEDOWN);
-            mMediaplayerHandler.sendEmptyMessage(FADEUP);
-
-            updateNotification();
-            if (!mIsSupposedToBePlaying) {
-                mIsSupposedToBePlaying = true;
-                notifyChange(PLAYSTATE_CHANGED);
-            }
-
-        } else if (mPlayListLen <= 0) {
-            // This is mostly so that if you press 'play' on a bluetooth headset
-            // without every having played anything before, it will still play
-            // something.
-            setShuffleMode(SHUFFLE_AUTO);
         }
     }
 
