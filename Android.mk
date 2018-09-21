64459c0aeddca0ef9105c8bab8ea93faccfa14ed
Raman Tenneti <rtenneti@google.com>
2018-09-20 17:33:28 -0700

    A simple music app - first cut at replacing exisitng music app with a simpler version as part of upding aops/apps.
    Bug: 112925066
    Change-Id: Ia9ef84066cb83369db7d00dd7f99d57eb6bcf32b

--- Android.mk
+++ Android.mk
@@ -5,6 +5,8 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
+LOCAL_STATIC_ANDROID_LIBRARIES := android-support-v7-appcompat
+
 LOCAL_PACKAGE_NAME := Music
 
 LOCAL_SDK_VERSION := current
