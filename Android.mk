4785bef59314fc0bdf010bec67c722eca451c484
Anton Hansson <hansson@google.com>
2019-01-11 14:58:52 +0000

    Move Music to /product
    This app is frequently overriden by proprietary apps
    on shipping devices, so move it to /product.
    
    Bug: 122353713
    Test: launch Music on aosp_arm64 emulator
    Change-Id: I58dccdb1daaa4683d564afffe67cda12b645c456

--- Android.mk
+++ Android.mk
@@ -7,6 +7,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := Music
 
+LOCAL_PRODUCT_MODULE := true
+
 LOCAL_SDK_VERSION := current
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
