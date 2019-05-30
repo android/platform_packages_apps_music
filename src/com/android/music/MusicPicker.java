15917c3d2ef31cf24cda5ed0de98b200733d137d
Raman Tenneti <rtenneti@google.com>
2019-05-30 15:08:12 -0700

    AOSP/Music - Bug fix to provide a dummy handler for Intent.ACTION_PICK.
    out/host/linux-x86/cts/android-cts$ tools/cts-tradefed
    
    Bug: 132217994, 132167668
    
    Test: manual - Ran the following tests on Pixel phone. Tested the email UI.
    
    cts-tf > run cts -m  CtsProviderTestCases -t android.provider.cts.MediaStoreIntentsTest
    5-30 14:55:25 I/ModuleListener: [1/8] android.provider.cts.MediaStoreIntentsTest#testPickAudioDir pass
    05-30 14:55:25 I/ModuleListener: [2/8] android.provider.cts.MediaStoreIntentsTest#testPickImageDir pass
    05-30 14:55:25 I/ModuleListener: [3/8] android.provider.cts.MediaStoreIntentsTest#testPickVideoDir pass
    05-30 14:55:25 I/ModuleListener: [4/8] android.provider.cts.MediaStoreIntentsTest#testViewAudioFile pass
    05-30 14:55:25 I/ModuleListener: [5/8] android.provider.cts.MediaStoreIntentsTest#testViewImageDir pass
    05-30 14:55:26 I/ModuleListener: [6/8] android.provider.cts.MediaStoreIntentsTest#testViewImageFile pass
    05-30 14:55:26 I/ModuleListener: [7/8] android.provider.cts.MediaStoreIntentsTest#testViewVideoDir pass
    05-30 14:55:26 I/ModuleListener: [8/8] android.provider.cts.MediaStoreIntentsTest#testViewVideoFile pass
    05-30 14:55:26 I/ConsoleReporter: [HT74D0200713] Starting arm64-v8a CtsProviderTestCases with 8 tests
    05-30 14:55:26 I/ConsoleReporter: [1/8 arm64-v8a CtsProviderTestCases HT74D0200713] android.provider.cts.MediaStoreIntentsTest#testPickAudioDir pass
    ....
    05-30 14:55:36 I/ConsoleReporter: [HT74D0200713] Starting armeabi-v7a CtsProviderTestCases with 8 tests
    05-30 14:55:36 I/ConsoleReporter: [1/8 armeabi-v7a CtsProviderTestCases HT74D0200713] android.provider.cts.MediaStoreIntentsTest#testPickAudioDir pass
    05-30 14:55:36 I/ConsoleReporter: [2/8 armeabi-v7a CtsProviderTestCases HT74D0200713] android.provider.cts.MediaStoreIntentsTest#testPickImageDir pass
    05-30 14:55:36 I/ConsoleReporter: [3/8 armeabi-v7a CtsProviderTestCases HT74D0200713] android.provider.cts.MediaStoreIntentsTest#testPickVideoDir pass
    05-30 14:55:36 I/ConsoleReporter: [4/8 armeabi-v7a CtsProviderTestCases HT74D0200713] android.provider.cts.MediaStoreIntentsTest#testViewAudioFile pass
    05-30 14:55:36 I/ConsoleReporter: [5/8 armeabi-v7a CtsProviderTestCases HT74D0200713] android.provider.cts.MediaStoreIntentsTest#testViewImageDir pass
    05-30 14:55:36 I/ConsoleReporter: [6/8 armeabi-v7a CtsProviderTestCases HT74D0200713] android.provider.cts.MediaStoreIntentsTest#testViewImageFile pass
    05-30 14:55:36 I/ConsoleReporter: [7/8 armeabi-v7a CtsProviderTestCases HT74D0200713] android.provider.cts.MediaStoreIntentsTest#testViewVideoDir pass
    05-30 14:55:36 I/ConsoleReporter: [8/8 armeabi-v7a CtsProviderTestCases HT74D0200713] android.provider.cts.MediaStoreIntentsTest#testViewVideoFile pass
    05-30 14:55:36 I/ConsoleReporter: [HT74D0200713] armeabi-v7a CtsProviderTestCases completed in 3s. 8 passed, 0 failed, 0 not executed
    05-30 14:55:40 I/SuiteResultReporter:
    ============================================
    ================= Results ==================
    =============== Consumed Time ==============
        armeabi-v7a CtsProviderTestCases: 3s
        arm64-v8a CtsProviderTestCases: 2s
    Total aggregated tests run time: 5s
    ============== Modules Preparation Times ==============
        arm64-v8a CtsProviderTestCases => prep = 2603 ms || clean = 449 ms
        armeabi-v7a CtsProviderTestCases => prep = 2608 ms || clean = 449 ms
    Total preparation time: 5s  ||  Total tear down time: 898 ms
    =======================================================
    =============== Summary ===============
    Total Run time: 47s
    2/2 modules completed
    Total Tests       : 16
    PASSED            : 16
    FAILED            : 0
    ============== End of Results ==============
    ============================================
    
    Change-Id: I2917f9da6be509b654ce045db9f4d5aa1b73a752

---
+++ src/com/android/music/MusicPicker.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.music;
+
+import android.app.ListActivity;
+import android.content.AsyncQueryHandler;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.database.CharArrayBuffer;
+import android.database.Cursor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.SectionIndexer;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.text.Collator;
+import java.util.Formatter;
+import java.util.Locale;
+
+/**
+ * Activity allowing the user to select a music track on the device, and
+ * return it to its caller.  The music picker user interface is fairly
+ * extensive, providing information about each track like the music
+ * application (title, author, album, duration), as well as the ability to
+ * previous tracks and sort them in different orders.
+ *
+ * <p>This class also illustrates how you can load data from a content
+ * provider asynchronously, providing a good UI while doing so, perform
+ * indexing of the content for use inside of a {@link FastScrollView}, and
+ * perform filtering of the data as the user presses keys.
+ */
+public class MusicPicker extends ListActivity implements View.OnClickListener {
+    static final boolean DBG = false;
+    static final String TAG = "MusicPicker";
+
+    /** Uri to the directory of all music being displayed. */
+    Uri mBaseUri;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        if (Intent.ACTION_GET_CONTENT.equals(getIntent().getAction())) {
+            mBaseUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        } else {
+            mBaseUri = getIntent().getData();
+        }
+        Log.w("MusicPicker", "Doesn't handle for data URI given to PICK action");
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle icicle) {
+        super.onSaveInstanceState(icicle);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {}
+
+    public void onClick(View v) {}
+}
